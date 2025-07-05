c07138703c4b78eeaeb835f312783ec44fe3dac0
praneeth4040 <praneethchakka23@gmail.com>
2025-07-05 12:19:48 +0530

    refactor: Format Supabase client initialization for improved readability

--- thoresport2/src/supabase.ts
+++ thoresport2/src/supabase.ts
@@ -1,3 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient("https://ltsombfuwcalqigfxccd.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0c29tYmZ1d2NhbHFpZ2Z4Y2NkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyOTk0MDUsImV4cCI6MjA2Njg3NTQwNX0.FzNBXrbO1HrBMVZVkuLi_8JFI37AoEIpl8LhASRR5As");
+export const supabase = createClient("https://ltsombfuwcalqigfxccd.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0c29tYmZ1d2NhbHFpZ2Z4Y2NkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyOTk0MDUsImV4cCI6MjA2Njg3NTQwNX0.FzNBXrbO1HrBMVZVkuLi_8JFI37AoEIpl8LhASRR5As");
